b8d00dfe93aea85753f0d3917c0cbbbde29d1251
raz-daniel <raz.daniel@gmail.com>
2025-04-08 10:23:57 +0300

    asd

---
+++ 2025-02-23-project-part4/06-social-network-server/src/controllers/auth/validator.ts
@@ -0,0 +1,11 @@
+import Joi from "joi";
+
+export const loginValidator = Joi.object({
+    username: Joi.string().alphanum().min(6).max(40).required(),
+    password: Joi.string().alphanum().min(6).max(40).required()
+})
+
+export const signupValidator = loginValidator.append({
+    name: Joi.string().alphanum().min(3).max(40).required()
+})
+
---
+++ 2025-02-23-project-part4/06-social-network-server/src/routers/authRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import validation from "../middlewares/validation";
+import { loginValidator, signupValidator } from "../controllers/auth/validator";
+import { login, signup } from "../controllers/auth/controller";
+
+const authRouter = Router()
+
+authRouter.post('/login', validation(loginValidator), login)
+authRouter.post('/signup', validation(signupValidator), signup)
+
+export default authRouter
