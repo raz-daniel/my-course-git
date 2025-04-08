c7c066a772e999c110a7928131b57682fa0b6618
raz-daniel <raz.daniel@gmail.com>
2025-04-08 09:52:58 +0300

    works?

---
+++ 2025-03-25 - Mongo/01-mongo/backend/src/model/post.ts
@@ -0,0 +1,54 @@
+import mongoose from "../db/mongoose"
+import { User } from "./user"
+
+interface PostComment {
+    body: string
+    userId: string
+    user: User
+    createdAt: Date
+}
+
+interface Post {
+    title: string
+    body: string
+    userId: string
+    user: User
+    comments: PostComment[]
+    createdAt: Date
+}
+
+const CommentSchema = new mongoose.Schema<PostComment>({
+    body: String,
+    userId: String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    createdAt: Date
+})
+
+const PostSchema = new mongoose.Schema<Post>({
+    title: String,
+    body: String,
+    userId: String,
+    user: {
+
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    comments: [CommentSchema],
+    createdAt: Date
+
+}, {
+    toObject: {
+        transform: function (doc, ret) {
+            ret.id = ret._id
+            delete ret._id
+            delete ret.__v
+        }
+
+    }
+
+})
+
+export const PostModel = mongoose.model<Post>('Post', PostSchema, 'posts')
